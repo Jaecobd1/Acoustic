be865f7fabb21d493d85f4fb612c3e755716b252
Jaecobd1 <jaecob.d1@gmail.com>
2022-06-08 13:16:50 -0400

    Need to fix PubSub

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,5 +1,8 @@
-const { UserInputError } = require('apollo-server-errors');
+const { AuthenticationError, UserInputError } = require('apollo-server-errors');
+
+const checkAuth = require('../../util/check-auth');
 const Post = require('../../models/Post');
+
 
 module.exports = {
     Mutation: {
@@ -23,7 +26,27 @@ module.exports = {
                 })
                 await post.save();
                 return post;
-            } else throw new UserInputError('Post not found')
+            } else throw new UserInputError('Post not found');
+        },
+        async deleteComment(_, { postId, commentId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comment.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                } 
+            }
+            else {
+                throw new UserInputError('Post not found');
+                }
         }
     }
 }
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-
+const pubsub = new PubSub()
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    constext: ({req}) => ({ req })
+    context: ({req}) => ({ req, pubsub })
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true })
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
         console.log("MongoDB Connected");
         return server.listen({ port: 5000 });
     })
-    .then(res => {
+    .then((res) => {
         console.log(`Server running at ${res.url}`);
 });
